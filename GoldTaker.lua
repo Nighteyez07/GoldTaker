e63fe2248f20ed6cb4020a4407a3ace94a6011ad
Nighteyez07 <Nighteyez07@gmail.com>
2019-03-21 15:21:24 -0700

    Add debug messaging and state toggle If player money doesn't update, delay and retry up to 5 attempts Only run if player has more than 50 mail

--- GoldTaker.lua
+++ GoldTaker.lua
@@ -1,8 +1,11 @@
 local frame, events = CreateFrame("Frame"), {};
 local deletedelay, t = 0.5, 0
-local mailBoxOpen, button2, waitForMail, doNothing, openAll, openMail, lastopened, stopOpening, onEvent, totalCollected
+local button2, waitForMail, doNothing, openAll, openMail, retryTakeMoney, takeMoney, lastopened, stopOpening, onEvent, totalCollected, attempt
+local totalMailCount, currentMailCount
 local _G = _G
 local limit = 0
+local debug = false
+local mailBoxOpen = false
 
 function events:MAIL_SHOW()
   mailBoxOpen = true;
@@ -24,20 +27,26 @@ for k, v in pairs(events) do
 end
 
 function openAll()
-  if GetInboxNumItems() == 0 then return end
+  currentMailCount, totalMailCount = GetInboxNumItems()
+  if currentMailCount <= 50 then 
+    print("GoldTaker: process cancelled, must have more than 50 mail to run")
+    return 
+  end
+  dPrint("totalMailCount = " .. totalMailCount)
   openMail(GetInboxNumItems())
 end
 
 function openMail(index)
-	if index <= 0 then return stopOpening() end
+  if index <= 0 then return stopOpening() end
 	local invoiceType, itemName, playerName, bid, buyout, deposit, consignment = GetInboxInvoiceInfo(index);
   local copperTotal = buyout + deposit - consignment;
   local total = math.floor((buyout + deposit - consignment) / 10000); --only handle in gold amounts
 
-	if total <= limit then
-		TakeInboxMoney(index)
-    print(("GoldTaker: Collected %dg %ds %dc"):format(copperTotal / 100 / 100, (copperTotal / 100) % 100, copperTotal % 100));
-    totalCollected = totalCollected + copperTotal;
+  print("index = " .. index .. ". invoiceType = ")
+
+  if total <= limit and invoiceType == "seller" then
+    attempt = 0
+    takeMoney(index) 
   else
     local items = GetInboxNumItems()
     if items > 1 and index < items + 1 then
@@ -53,6 +62,44 @@ function openMail(index)
 	else
 		return stopOpening()
 	end
+end
+
+function takeMoney(index) 
+  local playerMoney = GetMoney()
+  dPrint("takeMoney:index = " .. index)
+  dPrint("takeMoney:attempt = " .. attempt)
+
+  if attempt < 5 then
+    TakeInboxMoney(index)
+    --did we loot money
+    if playerMoney == GetMoney() then
+      dPrint("takeMoney:: no money looted")
+
+      attempt = attempt + 1
+      lastopened = index
+      t = 0
+      dPrint("takeMoney:lastopened = " .. lastopened)
+
+      frame:SetScript("OnUpdate", retryTakeMoney)
+
+      retryTakeMoney() --no idea why this is needed, but without this call then the OnUpdate for this function isn't actually called
+    else 
+      dPrint("Initial gold = " .. math.floor(playerMoney / 10000) .. " new total is " .. math.floor(GetMoney() / 10000))
+      print(("GoldTaker: Collected %dg %ds %dc"):format(copperTotal / 100 / 100, (copperTotal / 100) % 100, copperTotal % 100));
+      totalCollected = totalCollected + copperTotal;
+      attempt = 5  
+    end
+
+    dPrint("takeMoney:: END")
+  end
+end
+
+function retryTakeMoney(self, elapsed)
+  t = t + elapsed
+  if t > deletedelay then
+    frame:SetScript("OnUpdate", nil)
+    takeMoney(lastopened) 
+  end
 end
 
 function waitForMail(self, elapsed)
@@ -74,11 +121,42 @@ end
 
 SLASH_GOLDTAKER1, SLASH_GOLDTAKER2 = '/goldtaker', '/gt';
 local function handler(msg, editBox)
-  limit = tonumber(msg);
-  if limit == nil then
-    print("Limit must be a number, like '/gt 500'")
-    return stopOpening()
-  end
-  openAll()
+  slashArguments(msg)
 end
 SlashCmdList["GOLDTAKER"] = handler;
+
+function dPrint(str)
+  if str == nil then
+    return
+  end
+
+  if debug then
+    print("GoldTaker DEBUG:: " .. str)
+  end
+end
+
+function slashArguments(msg)
+  --if handle more statements, then swap to better way to run this
+  if msg == "" then
+    print("GoldTaker: No valid command found")
+  elseif tonumber(msg) then 
+    limit = tonumber(msg);
+    
+    if mailBoxOpen == false then
+      print("Mailbox must be open to retrieve gold")
+      return stopOpening()
+    elseif limit <= 0 then
+      print("Limit must be a number, like '/gt 500'")
+      return stopOpening()
+    else
+      openAll()
+    end
+  elseif string.lower(msg) == "debug" then
+    debug = not debug
+    print(debug)
+  else
+    print("GoldTaker: '" .. msg .. "' is not a valid command")
+  end
+
+end
+
